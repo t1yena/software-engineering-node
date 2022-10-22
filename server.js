762d22796aef22b03f088f4b1d5c4c7032857b69
Yena <t1yena@gmail.com>
2022-10-21 21:39:57 -0400

    test

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ import mongoose from "mongoose";
 var cors = require('cors');
 // build the connection string
 const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = "t1yena";
-const DB_PASSWORD = "yenawebdev";
-const HOST = "cluster0.bppa0ew.mongodb.net";
-const DB_NAME = "FSE";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const HOST = "cluster0.m8jeh.mongodb.net";
+const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
@@ -32,9 +32,9 @@ app.use(cors());
 app.get('/', (req, res) => res.send('Welcome!'));
 app.get('/add/:a/:b', (req, res) => res.send(req.params.a + req.params.b));
 // create RESTful Web service API
-// const userController = UserController.getInstance(app);
-// const tuitController = TuitController.getInstance(app);
-// const likesController = LikeController.getInstance(app);
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+const likesController = LikeController.getInstance(app);
 /**
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.
