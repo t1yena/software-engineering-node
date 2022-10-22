cb243cded5074dc4c82c678a3488022a546f723f
Yena <t1yena@gmail.com>
2022-10-21 21:54:11 -0400

    test

---
+++ dist/daos/TuitDao.js
@@ -0,0 +1,57 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @file Implements DAO managing data storage of tuits. Uses mongoose TuitModel
+ * to integrate with MongoDB
+ */
+const TuitModel_1 = __importDefault(require("../mongoose/tuits/TuitModel"));
+/**
+ * @class UserDao Implements Data Access Object managing data storage
+ * of Users
+ * @property {UserDao} userDao Private single instance of UserDao
+ */
+class TuitDao {
+    constructor() {
+        this.findAllTuits = () => __awaiter(this, void 0, void 0, function* () {
+            return TuitModel_1.default.find()
+                .populate("postedBy")
+                .exec();
+        });
+        this.findAllTuitsByUser = (uid) => __awaiter(this, void 0, void 0, function* () {
+            return TuitModel_1.default.find({ postedBy: uid })
+                .populate("postedBy")
+                .exec();
+        });
+        this.findTuitById = (uid) => __awaiter(this, void 0, void 0, function* () {
+            return TuitModel_1.default.findById(uid)
+                .populate("postedBy")
+                .exec();
+        });
+        this.createTuitByUser = (uid, tuit) => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.create(Object.assign(Object.assign({}, tuit), { postedBy: uid })); });
+        this.updateTuit = (uid, tuit) => __awaiter(this, void 0, void 0, function* () {
+            return TuitModel_1.default.updateOne({ _id: uid }, { $set: tuit });
+        });
+        this.deleteTuit = (uid) => __awaiter(this, void 0, void 0, function* () { return TuitModel_1.default.deleteOne({ _id: uid }); });
+    }
+}
+exports.default = TuitDao;
+TuitDao.tuitDao = null;
+TuitDao.getInstance = () => {
+    if (TuitDao.tuitDao === null) {
+        TuitDao.tuitDao = new TuitDao();
+    }
+    return TuitDao.tuitDao;
+};
+//# sourceMappingURL=TuitDao.js.map
--- dist/server.js
+++ dist/server.js
@@ -3,55 +3,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+/**
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
+ * <ul>
+ *     <li>users</li>
+ *     <li>tuits</li>
+ *     <li>likes</li>
+ * </ul>
+ *
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
+ */
 const express_1 = __importDefault(require("express"));
+const UserController_1 = __importDefault(require("./controllers/UserController"));
+const TuitController_1 = __importDefault(require("./controllers/TuitController"));
+const mongoose_1 = __importDefault(require("mongoose"));
+var cors = require('cors');
+// build the connection string
+const PROTOCOL = "mongodb+srv";
+const DB_USERNAME = "t1yena";
+const DB_PASSWORD = "yenawebdev";
+const HOST = "cluster0.bppa0ew.mongodb.net";
+const DB_NAME = "FSE";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+// connect to the database
+mongoose_1.default.connect(connectionString);
 const app = (0, express_1.default)();
-app.get('/', (req, res) => res.send('Welcome WebDev!'));
+app.use(express_1.default.json());
+app.use(cors());
+app.get('/', (req, res) => res.send('Welcome!'));
 app.get('/add/:a/:b', (req, res) => res.send(req.params.a + req.params.b));
-const PORT = 3000;
+// create RESTful Web service API
+const userController = UserController_1.default.getInstance(app);
+const tuitController = TuitController_1.default.getInstance(app);
+const PORT = 4000;
 app.listen(process.env.PORT || PORT);
-// /**
-//  * @file Implements an Express Node HTTP server. Declares RESTful Web services
-//  * enabling CRUD operations on the following resources:
-//  * <ul>
-//  *     <li>users</li>
-//  *     <li>tuits</li>
-//  *     <li>likes</li>
-//  * </ul>
-//  * 
-//  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
-//  * service
-//  */
-// import express, {Request, Response} from 'express';
-// import UserController from "./controllers/UserController";
-// import TuitController from "./controllers/TuitController";
-// import LikeController from "./controllers/LikeController";
-// import mongoose from "mongoose";
-// var cors = require('cors')
-// // build the connection string
-// const PROTOCOL = "mongodb+srv";
-// const DB_USERNAME = process.env.DB_USERNAME;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const HOST = "cluster0.m8jeh.mongodb.net";
-// const DB_NAME = "myFirstDatabase";
-// const DB_QUERY = "retryWrites=true&w=majority";
-// const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-// // connect to the database
-// mongoose.connect(connectionString);
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-// app.get('/', (req: Request, res: Response) =>
-//     res.send('Welcome!'));
-// app.get('/add/:a/:b', (req: Request, res: Response) =>
-//     res.send(req.params.a + req.params.b));
-// // create RESTful Web service API
-// const userController = UserController.getInstance(app);
-// const tuitController = TuitController.getInstance(app);
-// const likesController = LikeController.getInstance(app);
-// /**
-//  * Start a server listening at port 4000 locally
-//  * but use environment variable PORT on Heroku if available.
-//  */
-// const PORT = 4000;
-// app.listen(process.env.PORT || PORT);
 //# sourceMappingURL=server.js.map
