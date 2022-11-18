583144ba18a1676fd2216dae7b6c9d7d25ad5a03
Yena <t1yena@gmail.com>
2022-11-18 00:12:38 -0500

    implemented login

--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -42,7 +42,34 @@ const AuthenticationController = (app: Express) => {
         req.session.destroy();
         res.sendStatus(200);
     }
-    
+
+    const login = async (req, res) => {
+        const user = req.body;
+        const username = user.username;
+        const password = user.password;
+        const existingUser = await userDao
+          .findUserByUsername(username);
+      
+        if (!existingUser) {
+          res.sendStatus(403);
+          return;
+        }
+      
+        const match = await bcrypt
+          .compare(password, existingUser.password);
+      
+        if (match) {
+          existingUser.password = '*****';
+          req.session['profile'] = existingUser;
+          res.json(existingUser);
+        } else {
+          res.sendStatus(403);
+        }
+      };
+      
+    app.post("/api/auth/login", login);
+
+
     app.post("/api/auth/profile", profile);
     app.post("/api/auth/logout", logout);
 
