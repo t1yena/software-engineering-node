bf15f58a9df789a006b4f5598f8f7cf834c90976
Yena <t1yena@gmail.com>
2022-11-18 00:07:01 -0500

    implemented profile and logout

---
+++ controllers/auth-controller.ts
@@ -0,0 +1,51 @@
+import UserDao from "../daos/UserDao";
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+import {Express} from "express";
+
+
+const AuthenticationController = (app: Express) => {
+  
+    const userDao: UserDao = UserDao.getInstance();
+
+    const signup = async (req:any, res:any) => {
+        const newUser = req.body;
+        const password = newUser.password;
+        const hash = await bcrypt.hash(password, saltRounds);
+        newUser.password = hash;
+
+        const existingUser = await userDao
+            .findUserByUsername(req.body.username);
+        if (existingUser) {
+        res.sendStatus(403);
+        return;
+        } else {
+        const insertedUser = await userDao
+            .createUser(newUser);
+        insertedUser.password = '';
+        req.session['profile'] = insertedUser;
+        res.json(insertedUser);
+        }
+    }
+
+    const profile = (req, res) => {
+        const profile = req.session['profile'];
+        if (profile) {
+        profile.password = "";
+        res.json(profile);
+        } else {
+        res.sendStatus(403);
+        }
+    }
+    
+    const logout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    }
+    
+    app.post("/api/auth/profile", profile);
+    app.post("/api/auth/logout", logout);
+
+    app.post("/api/auth/signup", signup);
+}
+export default AuthenticationController;
