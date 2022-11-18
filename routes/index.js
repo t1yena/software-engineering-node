6145f4ecb3ac56d352716eac08ec221370aa569f
Yena <t1yena@gmail.com>
2022-11-18 15:46:01 -0500

    debug cors issue

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
 });
 
 module.exports = router;
