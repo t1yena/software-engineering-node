b4e84ced212f9a8ac9f614ed90b462260ef7f3c2
Yena <t1yena@gmail.com>
2022-11-19 17:00:49 -0500

    profile pic default value

--- daos/UserDao.ts
+++ daos/UserDao.ts
@@ -78,7 +78,8 @@
             userMongooseModel.password ?? '',
             userMongooseModel.firstName ?? '',
             userMongooseModel.lastName ?? '',
-            userMongooseModel.email ?? ''
+            userMongooseModel.email ?? '',
+            userMongooseModel.profilePhoto ?? ''
         );
      }
  
