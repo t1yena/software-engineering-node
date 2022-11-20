10f7c7d88d6d6a2bf2dc3356d9d6c1a53717b3b2
Yena <t1yena@gmail.com>
2022-11-20 01:09:59 -0500

    added like types to differentiate like and dislike

--- mongoose/likes/LikeSchema.ts
+++ mongoose/likes/LikeSchema.ts
@@ -7,5 +7,6 @@ import Like from "../../models/likes/Like";
 const LikeSchema = new mongoose.Schema<Like>({
     tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
     likedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    likeType: {type:String, enum:["LIKE","DISLIKE"]}
 }, {collection: "likes"});
 export default LikeSchema;
