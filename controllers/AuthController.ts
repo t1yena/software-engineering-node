539bcb17d2ae070f0b5f18366a5eba9559b65252
Yena <t1yena@gmail.com>
2022-12-05 13:31:15 -0500

    implemented auth controller

---
+++ controllers/AuthController.ts
@@ -0,0 +1,86 @@
+import UserDao from "../daos/UserDao";
+import {Express} from "express";
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+export default class AuthenticationController{
+    private static userDao: UserDao = UserDao.getInstance();
+    private static authenticationController: AuthenticationController | null = null;
+
+    public static getInstance = (app: Express): AuthenticationController => {
+        if (AuthenticationController.authenticationController === null) {
+            AuthenticationController.authenticationController = new AuthenticationController();
+
+            app.post("/api/auth/signup", AuthenticationController.authenticationController.signup);
+            app.post("/api/auth/profile", AuthenticationController.authenticationController.profile);
+            app.post("/api/auth/logout", AuthenticationController.authenticationController.logout);
+            app.post("/api/auth/login", AuthenticationController.authenticationController.login);
+        }
+        return AuthenticationController.authenticationController;
+    }
+    private constructor() {}
+
+    signup = async (req, res) => {
+        const newUser = req.body;
+        const password = newUser.password;
+        // Encrypt password before storing user
+        const hash = await bcrypt.hash(password, saltRounds);
+        newUser.password = hash;
+
+        const existingUser = await AuthenticationController.userDao.findUserByUsername(req.body.username);
+        if (existingUser) {
+            alert("user already exists");
+            res.sendStatus(403);
+            return;
+        } else {
+            const insertedUser = await AuthenticationController.userDao.createUser(newUser);
+            insertedUser.setPassword('');
+            // Store new user in session under profile attribute - now currently logged in user
+            req.session['profile'] = insertedUser;
+            res.json(insertedUser);
+        }
+    }
+
+    profile = (req, res) => {
+        const profile = req.session['profile'];
+        if (profile) {
+            profile.password = "";
+            res.json(profile);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+
+    logout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    }
+
+    login = async (req, res) => {
+        const user = req.body;
+        const username = user.username;
+        const password = user.password;
+
+        const existingUser = await AuthenticationController.userDao.findUserByUsername(username);
+
+        if (!existingUser) {
+            res.sendStatus(403);
+            return;
+        }
+
+        // Compare user object's password to hashed password passed from client
+        const match = await bcrypt.compare(password, existingUser.password);
+
+        if (match) {
+            // Password match: user object stored in profile attr in session - indicates user currently logged in
+            existingUser.password = '*****';
+            req.session['profile'] = existingUser;
+            res.json(existingUser);
+        } else {
+            // Password doesn't match: forbidden error 403
+            res.sendStatus(403);
+            console.log("Wrong password");
+        }
+    };
+}
--- models/users/User.ts
+++ models/users/User.ts
@@ -15,19 +15,34 @@ import mongoose from "mongoose";
   * @property {string} firstName first name of user
   * @property {string} lastName last name of user
   */
-export default interface User {
-    _id?: mongoose.Schema.Types.ObjectId,
-    username: string,
-    password: string,
-    email: string,
-    firstName?: string,
-    lastName?: string,
-    profilePhoto?: string,
-    headerImage?: string,
-    biography?: string,
-    dateOfBirth?: Date,
-    accountType?: AccountType,
-    maritalStatus?: MaritalStatus,
-    location?: Location,
-    salary?: number
+  export default class User {
+    private _id: string;
+    private username: string = '';
+    private password: string = '';
+    private email: string = '';
+    private firstName: string | null;
+    private lastName: string | null;
+    private profilePhoto: string;
+    private headerImage: string | null = null;
+    private biography: string | null = null;
+    private dateOfBirth: Date | null = null;
+    private accountType: AccountType | null = null;
+    private maritalStatus: MaritalStatus | null = null;
+    private location: Location | null = null;
+    private salary: number;
+    private joined: Date = new Date();
+
+    constructor(id:string, username: string, password: string, firstName: string | null, lastName: string | null, email: string, profilePhoto: string = 'emptyAvatar.png') {
+      this._id = id;
+      this.username = username;
+      this.password = password;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.profilePhoto = profilePhoto;
+    }
+
+    public setPassword(password:string) {
+      this.password = password;
+    }
 };
